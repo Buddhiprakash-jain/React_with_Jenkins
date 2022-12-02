b2cf85972846ab4b5bb4923132ed6cdf8bcdaedd
EC2 Default User <ec2-user@ip-172-31-30-126.ec2.internal>
2022-12-02 21:36:13 +0000

    ok

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Code change from GitHub new
+          Code changes by Github Webhook Triggers
         </p>
         <a
           className="App-link"
