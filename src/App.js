814287abac1c8fd9e49be9ef3f34335471cddda6
Buddhiprakash jain <71218910+Buddhiprakash-jain@users.noreply.github.com>
2022-12-03 03:41:51 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          By using Pipeline Triggers try
+          Hello Change!!
         </p>
         <a
           className="App-link"
