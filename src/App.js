9f76d7adb61545dc3399aa12da71471bdbad3f90
Buddhiprakash jain <71218910+Buddhiprakash-jain@users.noreply.github.com>
2022-12-04 18:42:02 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Backup one
+          Backup two
         </p>
         <a
           className="App-link"
