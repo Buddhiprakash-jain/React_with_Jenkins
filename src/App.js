4cc22dbe338d88a135ab7bc82b7442f1f65999ed
Buddhiprakash jain <71218910+Buddhiprakash-jain@users.noreply.github.com>
2022-12-22 10:55:20 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Backup latest
+          Hello react World!!
         </p>
         <a
           className="App-link"
